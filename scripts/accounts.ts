246be32031a586f0011fd12a15ef7c50972c8e6c
Adam Carr <addycarrr@gmail.com>
2023-02-21 09:48:50 -0500

    Blank solidity template

---
+++ scripts/accounts.ts
@@ -0,0 +1,7 @@
+ask("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
