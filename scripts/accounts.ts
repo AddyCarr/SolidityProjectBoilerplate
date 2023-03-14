7be763658bbfefd6cfcca8766a85530414b33e46
Adam Carr <addycarrr@gmail.com>
2023-03-15 09:47:37 +1100

    Added blank folders for organisation

--- scripts/accounts.ts
+++
@@ -1,7 +0,0 @@
-ask("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
